b74b9e2a5fb0b163c4e6adae5bf7babdf76dc0ce
Harsh Pandey <pandey73harsh@gmail.com>
2024-05-23 01:42:08 +0530

    more updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,45 @@
 import CodeEditor from "@/components/Editor/codeEditor";
 import Output from "@/components/Output/output";
 import Question from "@/components/Question/question";
-import Image from "next/image";
-import { cn } from "@/app/utils/cn";
+import Image from "next/image"; import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 
 export default function Home() {
-  
+
   return (
     <main className="min-h-screen flex w-screen bg-gray-950 ">
-
-      <div className=" w-1/2">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="min-h-screen max-w-screen"
+      >
+        <ResizablePanel defaultSize={50} >
         <Question />
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={50} >
+        <div className=" flex flex-col max-w-[50vw] h-full ">
+        <ResizablePanelGroup
+          direction="vertical"
+          className="min-h-screen max-w-full border"
+        >
+          <ResizablePanel defaultSize={50} >
+            <CodeEditor />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={50} >
+            <Output />
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
-      
-      <div className=" flex flex-col w-1/2 h-full ">
-        <CodeEditor />
-        <Output />
-      </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+
+
+
     </main>
   );
 }
